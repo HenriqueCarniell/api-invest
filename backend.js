155b5f5a29f3973bb6ee3a09571a74386ef339c8
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-06-11 14:39:57 -0300

    first commit

--- backend.js
+++ backend.js
@@ -11,6 +11,12 @@ const db = mysql.createPool({
     database: "invest",
     port: 3306
 });
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+  
   
 app.use(cors());
 app.use(express.json());
